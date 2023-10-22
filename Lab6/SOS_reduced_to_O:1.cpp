95cf5a34bf1fc79a4f84a92247e31bc688096c76
Kanya-bhatt <105928588+Kanya-bhatt@users.noreply.github.com>
2023-10-22 19:11:05 +0530

    second commit

--- Lab6/SOS_reduced_to_O:1.cpp
+++ Lab6/SOS_reduced_to_O:1.cpp
@@ -1,55 +1,75 @@
+#include<iostream>
 #include<bits/stdc++.h>
+
 using namespace std;
 
-bool Knapsack(vector<vector<int>> & mat, vector<int>&arr, int n, int sum){
-    for(int i = 0; i < n+1; i++){
-		mat[i][0] = 0;
-	} 
-	for(int j = 1; j <= sum; j++){
-		mat[0][j] = 0;
-	}
-    for(int i = 1; i < n+1; i++){
-		for(int j = 1; j <= sum; j++){
-			if(j < arr[i])
-				mat[i][j] = mat[i-1][j];
-			else
-				mat[i][j] = max(mat[i-1][j], mat[i-1][j - arr[i]] + arr[i]);
-		}
-	} 
-    if(mat[n][sum] >= sum)return true;
-    else return false;
+bool knapScack(vector<vector<int>> &mat, vector<int> &arr, int sum, int n){
+    
+    for(int i = 0; i < n; i++){
+        mat[i][0] = 0;
+        
+    }
+    
+    for(int i = 1; i <= sum; i++){
+        mat[0][i] = 0;
+    }
+    
+    for(int i = 1; i < n + 1; i++){
+        for(int j = 1; j <= sum; j++){
+            if(j < arr[i]){
+                mat[i][j] = mat[i-1][j];
+            }
+            else{
+                mat[i][j] = max(mat[i-1][j], mat[i-1][j - arr[i]] + arr[i]);
+            }
+        }
+    }
+    
+    if(mat[n][sum] >= sum){
+        return true;
+    }
+    return false;
+    
 }
 
+
 int main(){
-    int n;
-    cout << "Enter size : " << endl;
-    cin >> n;
-
-    int sum;
-    cout << "Enter sum : " << endl;
-    cin >> sum;
-    vector<vector<int>>mat(n+1, vector<int>(sum + 1));
-
-    set<int>s;
-    vector<int>arr(n + 1);
-    cout << "Enter set : " << endl;
     
+    int n;
+    cout << "enter the length of the set: " << n;
+    cin >> n;
+    
+    int sum;
+    cout << "enter the sum you want to achieve: " << n;
+    cin >> sum;
+    
+    // vector<vector<int>>mat(n+1, vector<int>(sum + 1));
+    vector<vector<int>> mat(n+1, vector<int>(sum+1));
+    
+    set<int> s;
+    
+    
+    cout << "enter the numbers you want to enter: " << endl;
     s.insert(0);
-    int temp;
-    for(int i = 1; i < n+1; i++){
-        cin >> temp;
-        s.insert(temp);
+    for(int i = 1; i < n + 1; i++){
+        int val;
+        cin >> val;
+        s.insert(val);
     }
-
-    int i = 1;
-    arr[0] = 0;
-    for(auto ele : s){
-        arr[i] = ele;
-        i++;
+    
+    vector<int> arr(s.begin(), s.end());
+    if(knapScack(mat, arr, sum, n) == true){
+        cout << "got ittt!!!" << endl;
     }
-    if(Knapsack(mat, arr, n, sum) == true)
-        printf("Hurehhh!!!");
-    else
-        printf("Oops!!!");
+    
+    else{
+        cout << "shittt!!" << endl;
+    }
+    
+    
     return 0;
+    
+    
+    
+    
 }
