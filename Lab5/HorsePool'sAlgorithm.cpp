d42f2754e65dc52433d40b503d974fd98d4859d6
Kanya-bhatt <105928588+Kanya-bhatt@users.noreply.github.com>
2023-10-22 13:58:35 +0530

    HorsePool'sAlgorithm.cpp

--- Lab5/HorsePool'sAlgorithm.cpp
+++ Lab5/HorsePool'sAlgorithm.cpp
@@ -1,60 +1,47 @@
-#include <bits/stdc++.h>
-#include <string>
-#include <iostream>
+#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-//shift table used to do string matching
-int printShift(string pattern, string text){
-	int m = pattern.length();
-	int n = text.length();
-	vector<int>  shift(26);
+
+int horsePool(string text, string pattern){
+    int n = text.length();
+    int m = pattern.length();
+    int shift[26];
+    for(int i = 0; i < 26; i++){
+        shift[i] = m;
+    }
     
-	for(int i = 0; i < 26; i++){
-    	shift[i] = m;
-	}
-	for(int i = 0; i <= m- 2; i++){
-    	if(pattern[i] == '_'){
-        	shift[pattern[i]] = 6;
+    for(int i = 0; i <= m - 2; i++){
+        if(pattern[i] == '_'){
+        	shift[pattern[i]] = m;
         	continue;
-    	}
-    	shift[pattern[i] - 'a'] = m - 1 - i;
-	}
+    	
+        }
+        shift[pattern[i] - 'a'] = m - i - 1;
+    }
     
-	int j = -1;
-	while(j + m <= n){
-    	if(pattern[m-1] == text[j + m - 1]){
-        	int i = m - 2;
-        	while(i >= 0 && pattern[i] == text[j+i]){//comparing all characters in the window
-            	i--;
-        	}
-        	if(i == -1){
-            	return j;
-        	}
-    	}
-    	j = j + shift[text[j + m - 1] - 'a'];
+    int j = 0;
+    while(j + m <= n){
+        if(pattern[m-1] == text[j + m - 1]){
+            int i = m - 2;
+            while(i >= 0 && text[j + i] == pattern[i]){
+                i--;
+            }
+            if(i == -1){
+                return j;
+            }
+        }
+        if((text[j + m - 1] - 'a') >= 0)
+    	    j = j + shift[text[j + m - 1] - 'a'];
+        else
+            j = j + m;
 	}
-	return n;
-
+	return -1;
+    
 }
 
-int main()
-{
-   string text;
-	string pattern;
-	cout << "enter the text: " << endl;
-	cin >> text;
-   
-	cout << "enter the pattern: " << endl;
-	cin >> pattern;
-   
+int main(){
+    string text = "jimsawmeinabarbershop";
+    string pattern = "barber";
     
-	// int n = text.length();
-	int m = pattern.length();
-    
-	int value = printShift(pattern, text);
-	cout << value << endl;
-    
-
-	return 0;
+    cout << horsePool(text, pattern) << endl;
 }
-
-
