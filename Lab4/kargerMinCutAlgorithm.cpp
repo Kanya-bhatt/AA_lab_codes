0cad3220078d8438badc3393db603edd3baf3aaa
Kanya-bhatt <105928588+Kanya-bhatt@users.noreply.github.com>
2023-10-22 11:42:15 +0530

    Update kargerMinCutAlgorithm.cpp

--- Lab4/kargerMinCutAlgorithm.cpp
+++ Lab4/kargerMinCutAlgorithm.cpp
@@ -1,72 +1,104 @@
-#include <iostream>
-#include <vector>
-#include <cstdlib>
-#include <ctime>
+#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
+#define N 4
+int G[N][N] = {{0, 1, 1, 1}, {1, 0, 0, 1}, {1, 0, 0, 1}, {1, 1, 1, 0}};
+int G1[N][N];
 
-
-vector<vector<int>> contractGraph(vector<vector<int>>& G, int u, int v, int n) {
-   
-    vector<vector<int>> G2(n - 1, vector<int>(n - 1, 0));
-
-    int idx = 0;
-    for (int i = 0; i < n; i++) {
-        if (i != u && i != v) {
-            int jdx = 0;
-            for (int j = 0; j < n; j++) {
-                if (j != u && j != v) {
-                    G2[idx][jdx] = G[i][j];
-                    jdx++;
-                }
-            }
-            idx++;
+int count_edges()
+{
+    int counter = 0;
+    int i, j;
+    for (i = 0; i < N; i++)
+    {
+        for (j = i + 1; j < N; j++)
+        {
+            if (G[i][j] == 1)
+                counter++;
         }
     }
-
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - 1; j++) {
-            if (i >= j) {
-                G2[i][j] = G2[j][i];
+    return counter;
+}
+void create_edge_matrix(int E[][2])
+{
+    int i, j;
+    int x = 0;
+    for (i = 0; i < N; i++)
+    {
+        for (j = i + 1; j < N; j++)
+        {
+            if (G[i][j] == 1)
+            {
+                E[x][0] = i;
+                E[x][1] = j;
+                x++;
             }
         }
     }
+}
+int find(int node, int parent[])
+{
 
-    return G2;
+    if (node == parent[node])
+        return node;
+
+    return parent[node] = find(parent[node], parent);
+}
+// Union function
+void Union(int u, int v, int parent[])
+{
+    u = find(u, parent);
+    v = find(v, parent);
+    parent[v] = u;
 }
 
-int minCut(vector<vector<int>>& G, int n, vector<vector<int>>& edge) {
-    if (n == 2) {
-        return G[0][1]; // Base case: return the remaining edge weight
-    }
-
-    int val = (rand() % edge.size());
-    int u = edge[val][0];
-    int v = edge[val][1];
-
-    // Recursively contract the graph G
-    vector<vector<int>> contractedGraph = contractGraph(G, u, v, n);
-
-    return minCut(contractedGraph, n - 1, edge);
-}
-
-int main() {
+int main()
+{
     srand(time(0));
+    int parent[N] = {0, 1, 2, 3};
+    int m = count_edges();
+    cout << "Number of Edges " << m << endl;
+    int i, j;
+    int n = N;
+    int edges[m][2];
+    create_edge_matrix(edges);
+    while (n > 2)
+    {
+        int u, v;
+       int e = rand() % m;
+        u = edges[e][0];
+        v = edges[e][1];
 
-    int n;
-    cout << "enter the number of vertices: " << endl;
-    cin >> n;
-    vector<vector<int>> G(n, vector<int>(n, 0));
+        int set1 = find(u, parent);
+        int set2 = find(v, parent);
 
-    int temp;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> temp;
-            G[i][j] = temp;
+       
+        if (set1 != set2)
+        {
+            cout << "Contracting-" << u << "," << v << endl;
+            Union(u, v, parent);
+            n--;
         }
     }
+    cout << "Edges needs to be removed - " << endl;
 
-    vector<vector<int>> edge = { {1, 2}, {1, 3}, {1, 4}, {2, 3}, {3, 4} };
-    cout << minCut(G, n, edge) << endl;
+    int ans = 0;
+    for (i = 0; i < N; i++)
+        cout << parent[i] << " ";
+    cout << endl;
+    for (int i = 0; i < m; i++)
+    {
 
+        int set1 = find(edges[i][0], parent);
+
+        int set2 = find(edges[i][1], parent);
+
+        if (set1 != set2)
+        {
+            cout << edges[i][0] << "-" << edges[i][1] << endl;
+            ans++;
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
